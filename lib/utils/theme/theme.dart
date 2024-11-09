7ba974c59d4c1ac57a4f9e1949536a85edef6b62
Abdulrhman Mohammed Shabaan <126196441+AbdulrhmanM0hamed@users.noreply.github.com>
2024-11-09 15:43:59 +0200

    finish sizes of app

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -12,38 +12,36 @@ class TAppTheme {
   TAppTheme._();
 
   static ThemeData lightTheme = ThemeData(
-      useMaterial3: true,
-      fontFamily: 'Poppins',
-      brightness: Brightness.light,
-      primaryColor: Colors.blue,
-      scaffoldBackgroundColor: Colors.white,
-      textTheme: TTextTheme.lightTextTheme,
-      elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme,
-      appBarTheme: TAppBarTheme.lightAppBarTheme,
-      bottomSheetTheme: TBottomSheetTheme.lightBottomSheetTheme,
-      checkboxTheme: TCheckboxTheme.lightCheckboxTheme,
-      chipTheme: TChipTheme.lightChipTheme,
-      inputDecorationTheme: TTextFormFieldTheme.lightInputDecorationTheme,
-      outlinedButtonTheme: OutlinedButtonThemeData(style: TOutlinedButtonTheme.lightOutlinedButtonTheme),
-
-      );
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TTextTheme.lightTextTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme,
+    appBarTheme: TAppBarTheme.lightAppBarTheme,
+    bottomSheetTheme: TBottomSheetTheme.lightBottomSheetTheme,
+    checkboxTheme: TCheckboxTheme.lightCheckboxTheme,
+    chipTheme: TChipTheme.lightChipTheme,
+    inputDecorationTheme: TTextFormFieldTheme.lightInputDecorationTheme,
+    outlinedButtonTheme: OutlinedButtonThemeData(
+        style: TOutlinedButtonTheme.lightOutlinedButtonTheme),
+  );
 
   static ThemeData darkTheme = ThemeData(
-      useMaterial3: true,
-      fontFamily: 'Poppins',
-      brightness: Brightness.dark,
-      primaryColor: Colors.blue,
-      scaffoldBackgroundColor: Colors.black,
-      textTheme: TTextTheme.darkTextTheme,
-      elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme,
-      appBarTheme: TAppBarTheme.darkAppBarTheme,
-      bottomSheetTheme: TBottomSheetTheme.darkBottomSheetTheme,
-      checkboxTheme: TCheckboxTheme.darkCheckboxTheme,
-      chipTheme: TChipTheme.darkChipTheme,
-      inputDecorationTheme: TTextFormFieldTheme.darkInputDecorationTheme,
-     outlinedButtonTheme: OutlinedButtonThemeData(style: TOutlinedButtonTheme.darkOutlinedButtonTheme),
-
-
-      
-      );
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme,
+    appBarTheme: TAppBarTheme.darkAppBarTheme,
+    bottomSheetTheme: TBottomSheetTheme.darkBottomSheetTheme,
+    checkboxTheme: TCheckboxTheme.darkCheckboxTheme,
+    chipTheme: TChipTheme.darkChipTheme,
+    inputDecorationTheme: TTextFormFieldTheme.darkInputDecorationTheme,
+    outlinedButtonTheme: OutlinedButtonThemeData(
+        style: TOutlinedButtonTheme.darkOutlinedButtonTheme),
+  );
 }
